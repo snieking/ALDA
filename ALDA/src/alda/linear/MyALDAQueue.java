d357ffe21b892c9ab1879486e8f14efbe59d15f8
snieking <viktorplane.sonie@gmail.comgit config --global user.email viktorplane.sonie@gmail.com>
2016-01-19 15:43:51 +0100

    Commit

--- ALDA/src/alda/linear/MyALDAQueue.java
+++ ALDA/src/alda/linear/MyALDAQueue.java
@@ -3,7 +3,7 @@ package alda.linear;
 import java.util.Collection;
 import java.util.Iterator;
 
-public class MyALDAQueue<E> implements ALDAQueue<E> {
+public class MyALDAQueue<E> implements Iterable<E>, ALDAQueue<E> {
 	
 	private static class Node<T> {
 		private final T data;
@@ -18,8 +18,46 @@ public class MyALDAQueue<E> implements ALDAQueue<E> {
 		}
 	}
 	
-	private Node<E> first;
-	private Node<E> last;
+	private class QueueIterator implements java.util.Iterator<E> {
+		private Node<E> current = head.next;
+		private int expectedCap = capacity;
+		private boolean canRemove = false;
+		
+		public Node<E> getCurrent() {
+			return current;
+		}
+
+		@Override
+		public boolean hasNext() {
+			return current != tail;
+		}
+
+		@Override
+		public E next() {
+			if(capacity != expectedCap)
+				throw new java.util.ConcurrentModificationException();
+			if(!hasNext())
+				throw new java.util.NoSuchElementException();
+			
+			E nextElement = current.data;
+			current = current.next;
+			canRemove = true;
+			return nextElement;
+		}
+		
+		public void remove() {
+			if(capacity != expectedCap)
+				throw new java.util.ConcurrentModificationException();
+			if(!canRemove)
+				throw new IllegalStateException();
+			
+			// TODO: Implement remove
+		}
+		
+	}
+	
+	private Node<E> head;
+	private Node<E> tail;
 	private int capacity;
 	private int currentSize = 0;
 	
@@ -31,21 +69,20 @@ public class MyALDAQueue<E> implements ALDAQueue<E> {
 
 	@Override
 	public Iterator<E> iterator() {
-		// TODO Auto-generated method stub
-		return null;
+		return new QueueIterator();
 	}
 
 	@Override
 	public void add(E element) { 						// Inspiration från: http://codereview.stackexchange.com/questions/62746/queue-implementation-using-a-linked-list
 		Node<E> n = new Node<E>(element);
 		if (isEmpty()) {
-			n.next = first;
-			first = n;
-			last = n;
+			n.next = head;
+			head = n;
+			tail = n;
 		} else {
-			last.next = n;
-			last = n;
-			last.next = null;
+			tail.next = n;
+			tail = n;
+			tail.next = null;
 		}
 		
 	}
@@ -114,8 +151,13 @@ public class MyALDAQueue<E> implements ALDAQueue<E> {
 	
 	@Override
 	public String toString() {
-		// TODO Iterate through list
-		return "";
+		String listString = "";
+		Iterator<E> iter = iterator();
+		while(iter.hasNext()) {
+			listString += ((QueueIterator) iter).getCurrent();
+			iter.next();
+		}
+		return listString;
 	}
 
 
