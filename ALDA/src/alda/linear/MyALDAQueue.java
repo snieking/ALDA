067311e5b855f79d3763b13a9172d6673314c285
snieking <viktorplane.sonie@gmail.comgit config --global user.email viktorplane.sonie@gmail.com>
2016-01-22 22:04:02 +0100

    Finished

--- ALDA/src/alda/linear/MyALDAQueue.java
+++ ALDA/src/alda/linear/MyALDAQueue.java
@@ -3,37 +3,40 @@ package alda.linear;
 import java.util.Collection;
 import java.util.Iterator;
 
+/**
+ * This file contains our implementation of a queue. 
+ * It has two inner classes. 
+ * The Node class which is constructed in the add method, and
+ * queueIterator which is created when needed and iterates over the queue.
+ * 
+ * @author Viktor Plane - viktorplane.sonie@gmail.com
+ * @author Olof Hofstedt - olof.hofstedt93@gmail.com
+ */
+
 public class MyALDAQueue<E> implements Iterable<E>, ALDAQueue<E> {
-	
+
 	private static class Node<T> {
 		private final T data;
 		private Node<T> next = null;
 		private boolean hasMoved = false;
-		
-		public Node (T data) {
-			this.data=data;
+
+		public Node(T data) {
+			this.data = data;
 		}
-		
+
 		public T printData() {
 			return data;
 		}
-		
-		@Override
-		public String toString() {
-			return data.toString();
-		}
 	}
-	
+
 	private class QueueIterator implements java.util.Iterator<E> {
 		private Node<E> current = head;
 		private int expectedCap = capacity;
-		private boolean canRemove = false;
-		
-		
+
 		public QueueIterator() {
-			
+
 		}
-		
+
 		public Node<E> getCurrent() {
 			return current;
 		}
@@ -45,29 +48,27 @@ public class MyALDAQueue<E> implements Iterable<E>, ALDAQueue<E> {
 
 		@Override
 		public E next() {
-			if(capacity != expectedCap)
+			if (capacity != expectedCap)
 				throw new java.util.ConcurrentModificationException();
-			if(!hasNext())
+			if (!hasNext())
 				throw new java.util.NoSuchElementException();
-			
-			Node<E> toReturn = new Node<E>(current.data);
 
+			Node<E> toReturn = new Node<E>(current.data);
 			current = current.next;
-			canRemove = true;
 			return toReturn.data;
 		}
-		
-		public void remove() { }
-		
+
+		public void remove() {}
+
 	}
-	
+
 	private Node<E> head;
 	private Node<E> tail;
 	private int capacity;
 	private int currentSize = 0;
-	
+
 	public MyALDAQueue(int cap) {
-		if(cap <= 0)
+		if (cap <= 0)
 			throw new IllegalArgumentException();
 		this.capacity = cap;
 	}
@@ -79,13 +80,12 @@ public class MyALDAQueue<E> implements Iterable<E>, ALDAQueue<E> {
 	}
 
 	@Override
-	public void add(E element) { 															// Inspiration från: http://codereview.stackexchange.com/questions/62746/queue-implementation-using-a-linked-list
-		if(element == null)
+	public void add(E element) { 
+		if (element == null)
 			throw new java.lang.NullPointerException();
-		
-		if(currentSize == capacity)
+		if (currentSize == capacity)
 			throw new java.lang.IllegalStateException();
-		
+
 		Node<E> n = new Node<E>(element);
 		if (isEmpty()) {
 			head = n;
@@ -98,25 +98,23 @@ public class MyALDAQueue<E> implements Iterable<E>, ALDAQueue<E> {
 			tail.next = null;
 			currentSize++;
 		}
-		
 	}
 
 	@Override
 	public void addAll(Collection<? extends E> c) {
-		if(c == null)
+		if (c == null)
 			throw new java.lang.NullPointerException();
-		
-		for(E element : c) {
+
+		for (E element : c) {
 			add(element);
 		}
-		
 	}
 
 	@Override
 	public E remove() {
-		if(isEmpty())
+		if (isEmpty())
 			throw new java.util.NoSuchElementException();
-		
+
 		Node<E> toRemove = head;
 		head = head.next;
 		currentSize--;
@@ -125,7 +123,7 @@ public class MyALDAQueue<E> implements Iterable<E>, ALDAQueue<E> {
 
 	@Override
 	public E peek() {
-		if(head != null)
+		if (head != null)
 			return (E) head.printData();
 		else
 			return null;
@@ -145,7 +143,7 @@ public class MyALDAQueue<E> implements Iterable<E>, ALDAQueue<E> {
 
 	@Override
 	public boolean isEmpty() {
-		if(currentSize == 0)
+		if (currentSize == 0)
 			return true;
 		else
 			return false;
@@ -153,7 +151,7 @@ public class MyALDAQueue<E> implements Iterable<E>, ALDAQueue<E> {
 
 	@Override
 	public boolean isFull() {
-		if(currentSize == capacity)
+		if (currentSize == capacity)
 			return true;
 		else
 			return false;
@@ -171,22 +169,20 @@ public class MyALDAQueue<E> implements Iterable<E>, ALDAQueue<E> {
 
 	@Override
 	public int discriminate(E e) {
-		if(e == null)
+		if (e == null)
 			throw new java.lang.NullPointerException();
-		
+
 		int timesMoved = 0;
-		
 		Node<E> compareNode = new Node<E>(e);
-		
 
 		Node<E> backupHead = head;
 		Node<E> backupTail = tail;
 		clear();
-		
+
 		Node<E> inspecting = backupHead;
-		
-		while(inspecting != null) {
-			if(inspecting.hasMoved == false && inspecting.printData().equals(compareNode.printData())) {
+
+		while (inspecting != null) {
+			if (inspecting.hasMoved == false && inspecting.printData().equals(compareNode.printData())) {
 				add(inspecting.data);
 				inspecting.hasMoved = true;
 				timesMoved++;
@@ -198,43 +194,41 @@ public class MyALDAQueue<E> implements Iterable<E>, ALDAQueue<E> {
 		Node<E> tempHead = head;
 		head = backupHead;
 		tail = backupTail;
-		
+
 		currentSize = 0;
-		
 		Node<E> notSearchingFor = head;
-		while(notSearchingFor != null) {
-			if(notSearchingFor.hasMoved == false && !(notSearchingFor.printData().equals(compareNode.printData()))) {
+		
+		while (notSearchingFor != null) {
+			if (notSearchingFor.hasMoved == false && !(notSearchingFor.printData().equals(compareNode.printData()))) {
 				add(notSearchingFor.data);
 				notSearchingFor.hasMoved = true;
 			} else
 				notSearchingFor = notSearchingFor.next;
 		}
-		
+
 		Node<E> temp = tempHead;
-		while(temp != null) {
+		while (temp != null) {
 			add(temp.data);
 			temp = temp.next;
 		}
-		
+
 		return timesMoved;
 	}
-	
-	
+
 	@Override
 	public String toString() {
 		String listString = "[";
-		
+
 		QueueIterator iter = (MyALDAQueue<E>.QueueIterator) iterator();
-		while(iter.getCurrent() != null) {
+		while (iter.getCurrent() != null) {
 			listString += ((QueueIterator) iter).getCurrent().printData();
 			iter.next();
-			if(iter.getCurrent() != null)
+			if (iter.getCurrent() != null)
 				listString += ", ";
 		}
+		
 		listString += "]";
-		return listString; 
-	} 
-
-
+		return listString;
+	}
 
 }
